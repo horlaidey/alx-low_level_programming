4b80462490df33426049c36616d7830ef21704f5
Horlaidey <salaudeenolaboy@gmail.com>
2023-03-16 23:42:37 +0000

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+	int j;
+	long int x;
+	long long int y;
+	float z;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(i));
+	printf("Size of an int: %d byte(s)\n", sizeof(j));
+	printf("Size of a long int: %d byte(s)\n", sizeof(x));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(y));
+	printf("Size of a float: &d byte(s)\n", sizeof(z));
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -6,4 +6,5 @@ result into another file \n
 ``3-name`` script compiles a ``C`` file and creates an executable named ``cisfun`` \n
 ``4-puts.c`` script is a C program that prints exactly ``"Programming is like a multilingual puzzle"`` \n
 ``5-printf.c`` script is a C program that prints exactly ``with proper grammar, but the outcome is a piece of art,`` \n
+``6-size.c`` script is a `C` program that prints the size of various types on the computer it is compiled \n
 
