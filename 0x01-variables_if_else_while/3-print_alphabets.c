58536f02e6b220940a1453c5e25de005b4c8583f
Horlaidey <salaudeenolaboy@gmail.com>
2023-03-20 14:11:19 +0000

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
