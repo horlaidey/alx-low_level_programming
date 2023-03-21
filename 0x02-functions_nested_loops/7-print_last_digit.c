e3e5824fef96c3434d41d68739810a85dbfc93e5
Horlaidey <salaudeenolaboy@gmail.com>
2023-03-21 23:12:17 +0000

    3 colors, 10 digits and 7 notes

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the ;last digit of a number.
+ *
+ * @a: an arbiutrary number.
+ *
+ * Return: The last digit of a number.
+ */
+int print_last_digit(int a)
+{
+	return (a % 10);
+}
