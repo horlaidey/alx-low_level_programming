c8ae529dbc03adc0994b32c93559efe3feb679c3
Horlaidey <salaudeenolaboy@gmail.com>
2023-03-21 13:44:45 +0000

    Prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-/*
- * main - Prints _putchar as a message.
+/**
+ * main - prints _putchar as a message
  *
- * Return: Always 0 (Success).
+ * Return: Always 0.
  */
 int main(void)
 {
