09420afebdb04119d71ddc40faa6c435ee93bd96
Horlaidey <salaudeenolaboy@gmail.com>
2023-03-22 17:49:54 +0000

    Fibonacci Numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,10 +2,10 @@
 /**
  * main - Prints the first 50 fibonacci numbers starting with 1 and 2
  *
- * Return: void
+ * Return: Always 0
  */
 
-void main(void)
+int main(void)
 {
 	int i = 1;
 	int j = 2;
@@ -20,4 +20,5 @@ void main(void)
 		printf(" ,%d", k);
 	}
 	printf("\n");
+	return (0);
 }
