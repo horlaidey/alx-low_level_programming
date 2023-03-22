2844a36b0608ba273e9c9e09c46c6dc97856f1e2
Horlaidey <salaudeenolaboy@gmail.com>
2023-03-22 01:19:38 +0000

    98 Battery Street,the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Prints number from n to 98
+ *
+ * @n: input number
+ *
+ * Return: void return
+ */
+
+void print_to_98(int n);
+{
+	int a;
+
+	for (a = n; a <= 98; a++)
+	{
+		printf("%d, ", a);
+	}
+}
