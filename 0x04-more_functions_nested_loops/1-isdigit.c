c6905a4ecdec54990725362c2323b2e6bc55f162
Horlaidey <salaudeenolaboy@gmail.com>
2023-03-23 23:55:56 +0000

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - checks if the input is digit
+ * @c: the tested integer input
+ *
+ * Return: 1 for digit and 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 97)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,5 +2,7 @@
 #define MAIN_H_
 int _putchar(char);
 int _isupper(int c);
+int _isdigit(int c);
+
 
 #endif
