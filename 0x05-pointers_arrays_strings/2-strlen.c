ee90092b5d1fc558de778f1f9aa19c59c1cf4734
Horlaidey <salaudeenolaboy@gmail.com>
2023-03-26 13:07:22 +0000

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strlen - evaluate the length of an input string
+ * @s: an arbitrary string input
+ * Return: the length of the string parameter
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
