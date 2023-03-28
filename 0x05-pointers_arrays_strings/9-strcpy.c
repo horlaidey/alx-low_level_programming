e44b40a5e5c06e9441c48a27c2e5397d3cacd24f
Horlaidey <salaudeenolaboy@gmail.com>
2023-03-28 11:21:29 +0000

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,19 +8,16 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i, j = 0;
+	int i = 0;
 
 	while (i >= 0)
 	{
-		if (src[i] == '\0')
+		*(dest + i) = *(src + i);
+		if (*(src + i) == '\0')
 		{
 			break;
 		}
 		i++;
-	}
-	for (; j <= i; j++)
-	{
-		*(dest + j) = *(src + j);
 	}
 	return (dest);
 }
