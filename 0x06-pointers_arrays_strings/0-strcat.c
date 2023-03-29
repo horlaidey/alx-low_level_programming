5c548f70251038a53c77ecbda4d352ac6e9e5c4e
Horlaidey <salaudeenolaboy@gmail.com>
2023-03-29 11:41:45 +0000

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * *_strcat - appends the src string to the dest string.
+ * @dest: the destination string
+ * @src: the source string
+ * Return: Destination string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (*(dest + i) == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+	while (count >= 0)
+	{
+		*(dest + (i + count)) = *(src + count);
+		if (*(src + count) == '\0')
+		{
+			break;
+		}
+		count++;
+	}
+	return (dest);
+}
