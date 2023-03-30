658ec3799071e49282016ea97e75196c9842df55
Horlaidey <salaudeenolaboy@gmail.com>
2023-03-30 11:00:08 +0000

    I did it :)

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,10 +11,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i >= 0)
+	while (i < n)
 	{
-		if (*(src + i) == '\0' || i >= n)
+		if (*(src + i) == '\0')
 		{
+			dest[i] = '\0';
 			break;
 		}
 		*(dest + i) = *(src + i);
