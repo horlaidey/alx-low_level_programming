3a4f89cac8cb4e6525aa12a6807367129d444d5b
Horlaidey <salaudeenolaboy@gmail.com>
2023-03-30 15:24:44 +0000

    Capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - Capitalize every first letter of
+ * a word in a string
+ * @s: input string
+ *
+ * Return: A refined string.
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] -= 32;
+		}
+		i++;
+		if ((s[i + 1] >= 97 && s[i + 1] <= 122)
+				&& (s[i] == 32 || s[i] == 44 || s[i] == 46))
+		{
+			s[i + 1] -= 32;
+		}
+		if (s[i] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+	return (s);
+}
