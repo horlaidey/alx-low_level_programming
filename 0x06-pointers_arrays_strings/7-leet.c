5d7d7607925d92eabb44a45756ecf4a313ae38fa
Horlaidey <salaudeenolaboy@gmail.com>
2023-03-30 16:50:34 +0000

    Mozart

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337.
+ * @s: Input string
+ * Return: strings of characters
+ */
+
+char *leet(char *s)
+{
+	int count = 0;
+	int i;
+
+	cap[5] = {97, 101, 111, 116, 108};
+	low[5] = {65, 69, 79, 84, 76};
+	check[5] = {4, 3, 0, 7, 1};
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*(s + count) == cap[i] || *(s + count) == low[i])
+			{
+				*(s + count) = check[i];
+			}
+		}
+		count++
+	}
+	return (s);
+}
