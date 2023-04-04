c936fd268f8bd0d28ba28d7d178bd09d1709de87
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-04 12:56:47 +0000

    Pointer to a pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - set the value of a pointer to achar
+ * @S: pointer to a string pointer
+ * @to: pointer to a character
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
