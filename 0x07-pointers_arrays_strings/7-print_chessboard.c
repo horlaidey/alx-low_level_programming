63c44e1e4015ccf389cc68c0427d6a4c5827d969
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-04 12:29:47 +0000

    Chess board

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -14,10 +14,10 @@ void print_chessboard(char (*a)[8])
 	{
 		if (i % 8 == 0 && i != 0)
 		{
-			m = i;
+			j = i;
 			_putchar('\n');
 		}
-		_putchar(a[i / 8][i - m]);
+		_putchar(a[i / 8][i - j]);
 	}
 	_putchar('\n');
 }
