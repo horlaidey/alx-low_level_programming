c76ba14ce5c4df1e8b6dc0e29bc838a86cf043bd
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-04 12:27:07 +0000

    Chess board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_chessboard - print chessboard
+ * @a: input character array
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j = 0;
+
+	for (i = 0; i < 64; i++)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			m = i;
+			_putchar('\n');
+		}
+		_putchar(a[i / 8][i - m]);
+	}
+	_putchar('\n');
+}
