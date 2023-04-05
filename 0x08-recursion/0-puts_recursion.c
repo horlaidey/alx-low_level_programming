17b100e0d9f1b428df720374f1174419f7740b7d
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-05 17:45:53 +0000

    Recursion correction

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,13 @@
 void _puts_recursion(char *s)
 {
 	int i = 0;
-	if (*(s + i) == '\0')
+	while (i >= 0)
 	{
-		_putchar('\n');
+		if (*(s + i) == '\0')
+		{
+			write(1, '\n', 1);
+		}
+		write(1, *(s + i), 1);
+		i++;
 	}
-	_putchar(*s);
-	i++;
-	_puts_recursion(*(s + i));
 }
