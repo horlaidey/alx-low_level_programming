70d23e8785f82a3e0f6868668d4681757dd54220
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-05 21:41:23 +0000

    recursive power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints the valu of x to the power of y
+ * @x: the base integer
+ * @y: the exponential integer
+ * Return: return the power of x
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0 || y == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
