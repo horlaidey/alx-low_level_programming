0adb23e9716fd9b5010b5995919259b75c583ff7
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-07 15:55:41 +0000

    Number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the number of argument with a new line
+ * @argc: input argument counts
+ * @argv: strings of input argument
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
