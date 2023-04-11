20bfeea581412d25b7df400b77b953b75cfc0b8b
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-11 16:09:36 +0000

    Malloc handled

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,10 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	ptr = malloc(size * sizeof(char));
 	if (ptr == NULL)
 	{
