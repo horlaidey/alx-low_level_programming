fca7c9cbaf26bc5401624a7c84fe5e0603a72df0
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-11 02:34:16 +0000

    Minimal numbers of coins for change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -18,39 +18,41 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	if (value < 0)
+	else
 	{
-		printf("0\n");
+		if (value < 0)
+		{
+			printf("0\n");
+		}
+		while (value > 0)
+		{
+			if (value >= 25)
+			{
+				i += value / 25;
+				value = value % 25;
+			}
+			else if (value < 25 && value >= 10)
+			{
+				i += value / 10;
+				value = value % 10;
+			}
+			else if (value < 10 && value >= 5)
+			{
+				i += value / 5;
+				value = value % 5;
+			}
+			else if (value < 5 && value >= 2)
+			{
+				i += value / 2;
+				value = value % 2;
+			}
+			else if (value == 1)
+			{
+				i += 1;
+				value = value % 1;
+			}
+		}
+		printf("%d\n", i);
+		return (0);
 	}
-	while (value > 0)
-	{
-		if (value >= 25)
-		{
-			i += value / 25;
-			value = value % 25;
-		}
-		else if (value < 25 && value >= 10)
-		{
-			i += value / 10;
-			value = value % 10;
-		}
-		else if (value < 10 && value >= 5)
-		{
-			i += value / 5;
-			value = value % 5;
-		}
-		else if (value < 5 && value >= 2)
-		{
-			i += value / 2;
-			value = value % 2;
-		}
-		else if (value == 1)
-		{
-			i += 1;
-			value = value % 1;
-		}
-	}
-	printf("%d\n", i);
-	return (0);
 }
-
