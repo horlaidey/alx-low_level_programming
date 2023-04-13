9559b403e6a7353654220bb313585178877eb03c
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-13 16:39:52 +0000

    string concat issue

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char *concat;
 
-	concat = string_nconcat("Best ", "School !!!", 3);
+	concat = string_nconcat("Best ", NULL, 6);
 	printf("%s\n", concat);
 	free(concat);
 	return (0);
