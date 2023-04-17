6af8e1d1ff5f6df25d09d702119cdc1e1834d2df
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-17 20:49:28 +0000

    struct description sorted

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog{ \
-	char *name; \
-	float age; \
-	char *owner; \
+/**
+ * struct dog - create a data type struct dog
+ * @name: A string data type for name
+ * @age: A float data type for age
+ * @owner: A string data type for owner
+ *
+ * Description: The new data type has three data structures
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
