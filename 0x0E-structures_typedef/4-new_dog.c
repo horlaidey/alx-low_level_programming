3e158c0ba8072394a3d0012292899b36e3b960e7
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-18 22:34:14 +0000

    New dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,9 +18,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 	{
-		return (NULL);
 		free(ptr);
-		exit(1);
+		return (NULL);
 	}
 	ptr->name = name;
 	ptr->age = age;
