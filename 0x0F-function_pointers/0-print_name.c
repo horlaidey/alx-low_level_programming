f03bbe4ffd534c78f14d3fad99211550befaaeb5
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-19 16:31:28 +0000

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints name base on the func pointer
+ * @name: string parameter
+ * @f: a function pointer acting as a callback
+ *
+ * Return: nothing
+ */
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
