e7eb1366428c5c4dcc02b744c88c03f53439bd26
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-21 01:27:55 +0000

    sum them all fixed

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <stdarg.h>
 #ifndef VAR_FUNC
 #define VAR_FUNC
+#include <stdio.h>
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char *);
 
