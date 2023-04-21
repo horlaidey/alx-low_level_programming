f4ca22c50d2d3aaeabb6f50e802fda8398674d19
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-21 01:24:07 +0000

    sum them all fixed

--- 0x10-variadic_functions/0-main.c
+++ 0x10-variadic_functions/0-main.c
@@ -12,7 +12,7 @@ int main(void)
 
 	sum = sum_them_all(2, 98, 1024);
 	printf("%d\n", sum);
-	sum = sum_them_all(4, 98, 1024, 402, -1024);
+	sum = sum_them_all(5, 98, 1024, 402, 0, -1024);
 	printf("%d\n", sum);
 	return (0);
 }
