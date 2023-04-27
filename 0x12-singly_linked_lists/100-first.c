ebe70d3a4cdc5c9788df3db79bb98669c73efc8b
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-27 23:45:51 +0000

    Before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**bmain - A function that runs before main function
+ *
+ * Return: Nothing
+ */
+
+void __attribute__((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow,
+		\nI bore my house upon my back!\n");
+}
