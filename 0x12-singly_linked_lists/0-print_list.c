c446be6dd6ed83e8c49ee4f126be0e8124dc841e
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-27 20:36:45 +0000

    Linked list print

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints the content of a linked list
+ * h: a pointer to a struct list
+ *
+ * Return: The number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned long int i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		i++;
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,9 @@
+#define LIST_H
+#ifndef LIST_H
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char);
+size_t print_list(const list_t *h);
+
+
+#endif
