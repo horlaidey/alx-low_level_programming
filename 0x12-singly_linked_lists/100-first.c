c2d7200d927a24f34b60d1db3435ab9b3a423863
Horlaidey <salaudeenolaboy@gmail.com>
2023-04-28 00:02:11 +0000

    before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * Return: Nothing
  */
 
-void __attribute__ ((constructor)) before_main()
+void __attribute__((constructor)) before_main()
 {
 	printf("You're beat! and yet, you must allow,
 		\nI bore my house upon my back!\n");
