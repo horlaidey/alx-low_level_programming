d9770b36c5d22e07484a12cbff6dd4017cd41d3e
Horlaidey <salaudeenolaboy@gmail.com>
2023-05-01 17:47:00 +0000

    requirement fail

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * pop_listint - deletes data from linked list
+ * @head: pointer to the head of list
+ *
+ * Return: return the deleted data;
+ */
+
 int pop_listint(listint_t **head)
 {
 	listint_t *ptr;
