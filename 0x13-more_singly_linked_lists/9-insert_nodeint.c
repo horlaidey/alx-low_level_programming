ec0bc238696d40f4292f329f8457c440bdc9630d
Horlaidey <salaudeenolaboy@gmail.com>
2023-05-01 20:55:58 +0000

    Insert at index fixed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,11 +12,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
+	if (idx == 0)
+	{
+		ptr = *head;
+		new->next = ptr;
+		*head = new;
+		return (*head);
+	}
 	ptr = *head;
 	i = 0;
 	while (ptr != NULL)
 	{
-		if (i == idx - 1)
+		if (i + 1 == idx)
 		{
 			new->next = ptr->next;
 			ptr->next = new;
