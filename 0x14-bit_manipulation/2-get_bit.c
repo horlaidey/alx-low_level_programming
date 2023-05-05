7bda321dc06fe648210b7ab949927cc95717c828
Horlaidey <salaudeenolaboy@gmail.com>
2023-05-05 00:21:39 +0000

    bits manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,11 +12,15 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned int j;
 
 	if (n == 0 && index < 64)
+	{
 		return (0);
+	}
 	for (i = 0; i <= 63; n >>= 1, i++)
 	{
 		if (index == i)
+		{
 			return (n & 1);
+		}
 	}
 	return (-1);
 }
