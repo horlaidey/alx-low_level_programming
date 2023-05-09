75ed3e6f33a419fbcd5e267f943052df0e0ae4c1
Horlaidey <salaudeenolaboy@gmail.com>
2023-05-09 23:03:21 +0000

    from a file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,26 @@
 #include "main.h"
 /**
+ * error_handle - check for error in file openings
+ * @file_from: First file from which we want to copy
+ * @file_to: second file into which we want to copy
+ * @argv: The argument vector
+ * Return: nothing
+ */
+void error_handle(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read file from %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
  * main - programs that copies files from one to the other
  * @argc: Number of arguments
  * @argv: arguments vector
@@ -18,16 +39,18 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	if (file_to == -1)
+	error_handle(file_from, file_to, argv);
+
+	num_chars = 1024;
+	while (num_chars == 1024)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		num_chars = read(file_from, buffer, 1024);
+		if (num_chars == -1)
+			error_handle(-1, 0, argv);
+		num_write = write(file_to, buffer, num_chars);
+		if (num_write == -1)
+			error_handle(0, -1, argv);
 	}
 	close_err = close(file_from);
 	if (close_err == -1)
