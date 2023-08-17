982a9a3e3878aa2259bd1743398d22f3bebcbc51
horlaidey <salaudeenolaboy@gmail.com>
2023-08-17 09:53:18 -0700

    Length of a linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len - return the length of a dlistint
+ * @h: pointer to dlistint 
+ * Return: return the length in size_t
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t length = 0;
+
+	while (h != NULL)
+	{
+		length += 1;
+		h = h->next;
+	}
+	return (length);
+}
