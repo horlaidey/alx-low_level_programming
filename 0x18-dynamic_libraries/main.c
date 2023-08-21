3c357ff0ef789edf7959aaf3fdbb827b11af71f0
horlaidey <salaudeenolaboy@gmail.com>
2023-08-21 04:57:58 -0700

    python use of dynamic c library

---
+++ 0x18-dynamic_libraries/main.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * add - addition of two integers
+ * @a: The first integer number
+ * @b: The second integer number
+ *
+ * Return: an integer sum of the two numbers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Substraction of two integer numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: An integer difference of the two numbers
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiplication of two integer numbers
+ * @a: The first integer number
+ * @b: The second integer number
+ *
+ * Return: An integer multiplication of the two number
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Division of two integers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The integer division of the two numbers
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Modulo division of two integer
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: The integer modulo of the two integer
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
